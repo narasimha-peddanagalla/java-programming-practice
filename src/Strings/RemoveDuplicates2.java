c437fa34780dd8f29ab3b9442dab6a792ae94abd
Peddanagalla Narasimha <narsynani116@gmail.com>
2026-01-29 17:39:04 +0530

    Added Solution for RemoveDuplicates Using Traditional Approach

---
+++ src/Strings/RemoveDuplicates2.java
@@ -0,0 +1,24 @@
+package Strings;
+/*Remove duplicate characters from a string
+Input:  "programming"
+Output: "progamin"
+*/
+
+//Traditional Approach - With StringBuilder
+public class RemoveDuplicates2 {
+	public static void main(String[] args) {
+		String str = "programming";
+		
+		StringBuilder unique = new StringBuilder();
+		
+		for(char ch : str.toCharArray()) {
+			
+			if(unique.indexOf(String.valueOf(ch)) == -1) {
+				unique.append(ch);
+			}
+		}
+		
+		System.out.println(unique.toString()); //progamin
+	}
+
+}
