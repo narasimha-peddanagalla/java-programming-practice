6801de85e409b1e629d4a97582166479bb228dfa
Peddanagalla Narasimha <narsynani116@gmail.com>
2026-01-29 22:07:26 +0530

    Added Approch-2 for Palindrome Check using Two-Pointer Method

---
+++ src/Strings/Palindrome2.java
@@ -0,0 +1,28 @@
+package Strings;
+//Check Given String is Palindrome or Not
+//Approach-2 : Two Pointer Method
+
+public class Palindrome2 {
+	public static void main(String[] args) {
+		
+		String str = "Nurses run";
+		
+		//Edge Case - Case insensitive & Avoid spaces
+		str = str.toLowerCase().replaceAll("\\s+","");
+		
+		int left = 0, right = str.length()-1;
+		
+		boolean isPalindrome = true;
+		
+		while(left<right){
+			
+			if(str.charAt(left) != str.charAt(right)) {
+				isPalindrome = false;
+				break;
+			}
+			left++;
+			right--;
+		}
+		System.out.println(isPalindrome ? "Palindrome":"Not Palindrome");
+	}
+}
