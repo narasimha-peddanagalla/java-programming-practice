5abfa8f2adc4ebe063cc90d51391ab412513d376
Peddanagalla Narasimha <narsynani116@gmail.com>
2026-02-02 14:14:26 +0530

    Finding Longest Sub-String Without Repeating Character - Bruteforce Approach

---
+++ src/Strings/LongestSubstring.java
@@ -0,0 +1,42 @@
+package Strings;
+import java.util.HashSet;
+/*
+Find the length of the longest substring without repeating characters.
+A substring must be continuous and contain only unique characters.
+Input: String str = "abcabcbb";
+Output: Longest substring length: 3 (The substring is "abc")
+*/
+
+//Approach-1 : Bruteforce - Time Complexity: O(n)^2
+public class LongestSubstring {
+	public static void main(String[] args) {
+		String str = "abcabcbb";
+		
+		String longest = "";
+				
+		for(int i=0;i<str.length();i++) {
+			
+			HashSet<Character> set = new HashSet<>();
+			String current = "";
+			
+			for(int j=i;j<str.length();j++) {
+				
+				if(set.contains(str.charAt(j))) {
+					break;
+				}
+				
+				set.add(str.charAt(j));
+				
+				current += str.charAt(j);
+				
+				if(current.length() > longest.length()) {
+					longest = current;
+				}
+			
+			}
+		}
+		System.out.println("Longest Sub String:"+longest);
+		System.out.println("Length:"+longest.length());
+	}
+	
+}
